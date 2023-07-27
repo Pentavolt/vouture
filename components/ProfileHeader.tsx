45d8208e935c17eaf8cc01602079c1295454780e
Pentavolt <amorphousei@gmail.com>
2023-07-27 09:24:40 +0200

    feature: update cache on follow and unfollow

--- components/ProfileHeader.tsx
+++ components/ProfileHeader.tsx
@@ -1,12 +1,129 @@
-import { Avatar, H2, Paragraph, XGroup, XStack, YStack, Text } from "tamagui";
-import { User } from "../generated/gql/graphql";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  Avatar,
+  H2,
+  Paragraph,
+  XGroup,
+  XStack,
+  YStack,
+  Text,
+  Button,
+} from "tamagui";
+import {
+  CreateFollowDocument,
+  DeleteFollowDocument,
+  GetUserDocument,
+  User,
+} from "../generated/gql/graphql";
 import Badge from "./Badge";
+import { useAuth } from "../lib/hooks";
+import { useMutation } from "@apollo/client";
+import { useRef } from "react";
 
 interface ProfileHeaderProps {
   user: User;
 }
 
 export default function ProfileHeader({ user }: ProfileHeaderProps) {
+  const { user: me } = useAuth();
+  const [follow] = useMutation(CreateFollowDocument);
+  const [unfollow] = useMutation(DeleteFollowDocument);
+  const isFollowing = useRef(
+    user.followers.some((follow) => follow.followerId === me?.id)
+  );
+
+  const onPress = () => {
+    if (!me) return;
+    if (isFollowing.current) {
+      unfollow({
+        variables: {
+          where: {
+            follow: {
+              followerId: me.id,
+              followingId: user.id,
+            },
+          },
+        },
+        update(cache) {
+          // Remove follow from the target user.
+          cache.updateQuery(
+            { query: GetUserDocument, variables: { where: { id: user.id } } },
+            (data) => {
+              if (!data?.user) return undefined;
+              return {
+                user: {
+                  ...data.user,
+                  followers: data.user.followers.filter(
+                    (follow) => follow.followerId !== me.id
+                  ),
+                },
+              };
+            }
+          );
+
+          // Remove following from current user.
+          cache.updateQuery(
+            { query: GetUserDocument, variables: { where: { id: me.id } } },
+            (data) => {
+              if (!data?.user) return undefined;
+              return {
+                user: {
+                  ...data.user,
+                  following: data.user.following.filter(
+                    (follow) => follow.followingId !== user.id
+                  ),
+                },
+              };
+            }
+          );
+        },
+      });
+
+      isFollowing.current = false;
+    } else {
+      follow({
+        variables: {
+          data: {
+            follower: { connect: { id: me.id } },
+            following: { connect: { id: user.id } },
+          },
+        },
+        update(cache, { data: mutation }) {
+          if (!mutation) return;
+          // Add follower to target user.
+          cache.updateQuery(
+            { query: GetUserDocument, variables: { where: { id: user.id } } },
+            (data) => {
+              if (!data?.user) return undefined;
+              return {
+                user: {
+                  ...data.user,
+                  followers: [...data.user.followers, mutation.createOneFollow],
+                },
+              };
+            }
+          );
+
+          // Add follow to current user.
+          cache.updateQuery(
+            { query: GetUserDocument, variables: { where: { id: me.id } } },
+            (data) => {
+              if (!data?.user) return undefined;
+              return {
+                user: {
+                  ...data.user,
+                  following: [...data.user.following, mutation.createOneFollow],
+                },
+              };
+            }
+          );
+        },
+      });
+
+      isFollowing.current = true;
+    }
+  };
+
   return (
     <YStack
       space
@@ -77,6 +194,19 @@ export default function ProfileHeader({ user }: ProfileHeaderProps) {
           </YStack>
         </XGroup.Item>
       </XGroup>
+      {user.id !== me?.id && (
+        <Button
+          onPress={onPress}
+          width={"100%"}
+          icon={
+            <Ionicons
+              name={isFollowing.current ? "person-remove" : "person-add"}
+            />
+          }
+        >
+          {isFollowing.current ? "Unfollow" : "Follow"}
+        </Button>
+      )}
     </YStack>
   );
 }
