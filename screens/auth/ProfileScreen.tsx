3cc80bc5f982588f1664eeea24dbb40a3d8b76b7
Pentavolt <amorphousei@gmail.com>
2023-07-30 16:45:15 +0200

    feature: handle private profiles and blocks

--- screens/auth/ProfileScreen.tsx
+++ screens/auth/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
-import { useAuth } from "../../lib/hooks";
+import { useAuth, useBottomSheetBack } from "../../lib/hooks";
 import { UserStackScreenProps } from "../../lib/navigation/types";
 import { FlashList } from "@shopify/flash-list";
 import { useQuery } from "@apollo/client";
@@ -10,9 +10,11 @@ import {
   SortOrder,
   User,
 } from "../../generated/gql/graphql";
-import { Paragraph, Text, YStack } from "tamagui";
+import { Paragraph, Text, View, YStack } from "tamagui";
 import PostPreview from "../../components/post/PostPreview";
 import ProfileHeader from "../../components/ProfileHeader";
+import { useEffect, useState } from "react";
+import ProfileActionSheet from "../../components/ProfileActionSheet";
 
 // TODO: The props should be HomeStackScreenProps as well.
 
@@ -20,24 +22,47 @@ export default function ProfileScreen({
   navigation,
   route,
 }: UserStackScreenProps<"Profile">) {
+  const [open, setOpen] = useState<boolean>(false);
   const { user } = useAuth();
-  const currentUser = route.params ? route.params.user.id : user?.id;
+  const currentUserId = route.params ? route.params.user.id : user?.id;
 
-  const { data, refetch } = useQuery(GetUserDocument, {
-    variables: { where: { id: currentUser } },
+  useBottomSheetBack(open, () => setOpen(false));
+  const {
+    data,
+    loading: userLoading,
+    refetch,
+  } = useQuery(GetUserDocument, {
+    variables: { where: { id: currentUserId } },
   });
 
   const {
     data: content,
-    loading,
+    loading: postsLoading,
     fetchMore,
   } = useQuery(PostsDocument, {
     variables: {
-      where: { userId: { equals: currentUser } },
+      where: { userId: { equals: currentUserId } },
       orderBy: { createdAt: SortOrder["Desc"] },
       take: 20,
     },
   });
+
+  useEffect(() => {
+    if (!data?.user || !user) return;
+    if (user.id !== data.user.id) {
+      navigation.setOptions({
+        headerRight: () => (
+          <Ionicons
+            size={26}
+            style={{ marginHorizontal: 12 }}
+            name="ellipsis-horizontal"
+            color={"black"}
+            onPress={() => setOpen(true)}
+          />
+        ),
+      });
+    }
+  }, [userLoading]);
 
   const renderItem = ({ item }: { item: Post }) => (
     <PostPreview
@@ -46,59 +71,73 @@ export default function ProfileScreen({
     />
   );
 
-  if (loading || !data?.user) return <Text>Loading...</Text>;
-  if (
+  if (postsLoading || userLoading || !data?.user) {
+    return <Text>Loading...</Text>;
+  }
+
+  const isBlocked =
+    data.user.blocked.some((block) => block.blockerId === user?.id) ||
+    data.user.blocker.some((block) => block.blockedId === user?.id);
+
+  const isPrivate =
     data.user.id !== user?.id &&
     data.user.isPrivate &&
-    !data.user.followers.some((follow) => follow.followerId === user?.id)
-  ) {
-    return (
-      <YStack flex={1}>
-        <ProfileHeader user={data.user as User} />
+    !data.user.followers.some((follow) => follow.followerId === user?.id);
+
+  return (
+    <View flex={1}>
+      <ProfileActionSheet
+        open={open}
+        onOpenChange={setOpen}
+        currentUser={user as User}
+        viewedUser={data.user as User}
+      />
+      <FlashList<Post>
+        ListHeaderComponent={<ProfileHeader user={data.user as User} />}
+        renderItem={renderItem}
+        onRefresh={refetch}
+        refreshing={postsLoading}
+        numColumns={3}
+        keyExtractor={(_, idx) => idx.toString()}
+        data={isBlocked || isPrivate ? [] : (content?.posts as Post[])}
+        estimatedItemSize={200}
+        onEndReachedThreshold={0.5}
+        onEndReached={async () => {
+          if (isBlocked || isPrivate) return;
+          const lastItemId = content?.posts[content.posts.length - 1]?.id;
+          await fetchMore({
+            variables: {
+              ...(lastItemId ? { cursor: { id: lastItemId }, skip: 1 } : {}),
+            },
+            updateQuery: (previousQueryResult, { fetchMoreResult }) => {
+              return {
+                posts: [...previousQueryResult.posts, ...fetchMoreResult.posts],
+              };
+            },
+          });
+        }}
+      />
+      {(isBlocked || isPrivate) && (
         <YStack
+          flex={1}
           space
           paddingHorizontal={"$7"}
           flexGrow={1}
           alignItems="center"
           justifyContent="center"
         >
-          <Ionicons name="lock-closed" size={40} />
+          <Ionicons name={isBlocked ? "eye-off" : "lock-closed"} size={40} />
           <Paragraph
             fontFamily={"$span"}
             color={"$gray7Dark"}
             textAlign="center"
           >
-            This user has a private profile. To see their posts, request to
-            follow them.
+            {isBlocked
+              ? "You are not allowed to view this user's posts."
+              : "This profile is private. Only followers can see their posts."}
           </Paragraph>
         </YStack>
-      </YStack>
-    );
-  }
-
-  return (
-    // TODO: Try design with space between images.
-    <FlashList<Post>
-      ListHeaderComponent={<ProfileHeader user={data.user as User} />}
-      renderItem={renderItem}
-      onRefresh={refetch}
-      refreshing={loading}
-      numColumns={3}
-      keyExtractor={(_, idx) => idx.toString()}
-      data={content?.posts as Post[]}
-      estimatedItemSize={200}
-      onEndReachedThreshold={0.5}
-      onEndReached={async () => {
-        const lastItemId = content?.posts[content.posts.length - 1].id;
-        await fetchMore({
-          variables: { cursor: { id: lastItemId }, skip: 1 },
-          updateQuery: (previousQueryResult, { fetchMoreResult }) => {
-            return {
-              posts: [...previousQueryResult.posts, ...fetchMoreResult.posts],
-            };
-          },
-        });
-      }}
-    />
+      )}
+    </View>
   );
 }
