454f82ed01ae0740e068acf0518dec9c6e2ef013
Pentavolt <amorphousei@gmail.com>
2023-08-15 16:49:41 +0200

    fix: remake screen design

--- screens/media/PreviewScreen.tsx
+++ screens/media/PreviewScreen.tsx
@@ -1,14 +1,16 @@
 import { Ionicons } from "@expo/vector-icons";
-import { useCallback, useState } from "react";
-import { useAuth } from "../../lib/hooks";
+import { useState } from "react";
+import { useAuth, useBottomSheetBack } from "../../lib/hooks";
 import { CameraStackScreenProps } from "../../lib/navigation/types";
 import {
   Button,
+  Heading,
+  Input,
   ListItem,
   Separator,
+  Sheet,
   Spinner,
   Switch,
-  Text,
   TextArea,
   View,
   XStack,
@@ -16,19 +18,20 @@ import {
   YStack,
 } from "tamagui";
 import { CommonActions } from "@react-navigation/native";
-import { useLazyQuery, useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import {
   CreatePostDocument,
+  QueryMode,
   Topic,
   TopicsDocument,
   UploadDocument,
 } from "../../generated/gql/graphql";
 import { ReactNativeFile } from "apollo-upload-client";
 import FastImage from "react-native-fast-image";
-import KeyboardAwareScrollView from "../../components/post/KeyboardAwareScrollView";
-import TopicInput from "../../components/post/TopicInput";
-import { FlashList } from "@shopify/flash-list";
+import { FlashList, ListRenderItemInfo } from "@shopify/flash-list";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { Keyboard, Pressable } from "react-native";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function PreviewScreen({
   navigation,
@@ -36,13 +39,25 @@ export default function PreviewScreen({
 }: CameraStackScreenProps<"Preview">) {
   const [post] = useMutation(CreatePostDocument);
   const [upload] = useMutation(UploadDocument);
-  const [fetch, { data }] = useLazyQuery(TopicsDocument);
   const [caption, setCaption] = useState<string>("");
   const [uploading, setUploading] = useState<boolean>(false);
   const [topicNames, setTopicNames] = useState<string[]>([]);
   const [checked, setChecked] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(false);
+  const { data, refetch } = useQuery(TopicsDocument);
   const { user } = useAuth();
   const { photo, tags } = route.params;
+
+  useBottomSheetBack(open, () => setOpen(false));
+  const debounce = useDebouncedCallback(
+    (value: string) =>
+      refetch({
+        where: {
+          name: { startsWith: value, mode: QueryMode["Insensitive"] },
+        },
+      }),
+    1000
+  );
 
   const onPress = async () => {
     if (!photo) return;
@@ -97,144 +112,108 @@ export default function PreviewScreen({
     });
   };
 
-  // const renderItem = useCallback(
-  //   ({ item }: { item: Topic }) => (
-  //     <TouchableOpacity
-  //       style={{
-  //         paddingHorizontal: 10,
-  //         backgroundColor: "red",
-  //         height: 40,
-  //         justifyContent: "center",
-  //       }}
-  //       onPress={() => setTopicNames((curr) => [...curr, item.name])}
-  //     >
-  //       <Text color={"black"}>#{item.name}</Text>
-  //     </TouchableOpacity>
-  //   ),
-  //   []
-  // );
-
-  // TODO: Implement autocomplete
+  const renderItem = ({ item }: ListRenderItemInfo<Topic>) => (
+    <TouchableOpacity
+      style={{ height: 50 }}
+      onPress={() => {
+        setTopicNames((curr) =>
+          curr.length >= 10 ? curr : [item.name, ...curr]
+        );
+      }}
+    >
+      <Heading color={"black"}>{item.name}</Heading>
+    </TouchableOpacity>
+  );
 
   return (
-    <KeyboardAwareScrollView>
-      <YStack space padding="$5">
-        <FastImage
-          source={{ uri: photo }}
-          resizeMode="cover"
-          style={{
-            height: 300,
-            width: "100%",
-            alignSelf: "center",
-            borderRadius: 10,
-          }}
-        />
-        <TextArea
-          maxLength={200}
-          minHeight={100}
-          backgroundColor={"$gray12"}
-          textAlignVertical="top"
-          color={"black"}
-          borderColor={"$gray12"}
-          focusStyle={{ borderColor: "$gray12" }}
-          onChangeText={(text) => setCaption(text)}
-          value={caption}
-          placeholder="Add a caption..."
-        />
-        <XStack
-          space="$1"
-          paddingVertical="$2"
-          paddingHorizontal="$3"
-          borderRadius={10}
-          flexWrap="wrap"
-          backgroundColor={"$gray12"}
-          borderColor={"$gray12"}
-          alignItems="center"
-          borderWidth={1}
-        >
-          {topicNames.map((topicName, idx) => (
-            <Button
-              marginBottom={"$2"}
-              key={idx}
-              size={"$2"}
-              onPress={() =>
-                setTopicNames((curr) =>
-                  curr.filter((_, index) => index !== idx)
-                )
-              }
-              icon={<Ionicons name="close" color={"white"} />}
-              color={"black"}
-              textProps={{
-                color: "white",
-                numberOfLines: 1,
+    <Pressable onPress={Keyboard.dismiss} style={{ flex: 1 }}>
+      <YStack
+        flex={1}
+        space="$9"
+        paddingVertical="$5"
+        justifyContent="space-between"
+        paddingHorizontal="$3"
+      >
+        <YStack space>
+          <XStack>
+            <FastImage
+              source={{ uri: photo }}
+              resizeMode="cover"
+              style={{
+                aspectRatio: 9 / 16,
+                width: 100,
+                borderTopLeftRadius: 5,
+                borderBottomLeftRadius: 5,
               }}
-            >
-              {topicName}
-            </Button>
-          ))}
-          <TopicInput
-            onAdd={(name) => setTopicNames((curr) => [...curr, name])}
-            onRemove={() =>
-              setTopicNames((curr) =>
-                curr.filter((_, idx) => idx !== topicNames.length - 1)
-              )
-            }
-            onChange={async (value) => {
-              await fetch({
-                variables: { where: { name: { startsWith: value } } },
-              });
-            }}
-          />
-        </XStack>
-        {/* {!!data?.topics.length && (
-          <View minHeight={20}>
-            <FlashList<Topic>
-              data={data?.topics as Topic[]}
-              renderItem={renderItem}
-              estimatedItemSize={20}
             />
-          </View>
-        )} */}
-        <YGroup separator={<Separator />}>
-          <YGroup.Item>
-            <ListItem
-              title={"Allow comments"}
-              icon={<Ionicons name="chatbox" />}
-              subTitle={"Allow other users to comment on this post."}
-              iconAfter={
-                <Switch
-                  size={"$3"}
-                  checked={checked}
-                  backgroundColor={checked ? "#FE9F10" : "$gray6Dark"}
-                  onCheckedChange={() => setChecked((curr) => !curr)}
-                >
-                  <Switch.Thumb animation="bouncy" />
-                </Switch>
-              }
+            <TextArea
+              flexGrow={1}
+              fontFamily={"$span"}
+              borderRadius={0}
+              borderTopRightRadius={5}
+              borderBottomRightRadius={5}
+              maxLength={200}
+              minHeight={100}
+              backgroundColor={"white"}
+              textAlignVertical="top"
+              color={"black"}
+              borderColor={"white"}
+              focusStyle={{ borderColor: "white" }}
+              onChangeText={(text) => setCaption(text)}
+              value={caption}
+              placeholder="Add a caption..."
             />
-          </YGroup.Item>
-          <YGroup.Item>
-            <ListItem
-              hoverTheme
-              pressTheme
-              title="Location"
-              subTitle="Add a location"
-              icon={<Ionicons name="location" />}
-              iconAfter={<Ionicons name="chevron-forward" />}
-            />
-          </YGroup.Item>
-          <YGroup.Item>
-            <ListItem
-              hoverTheme
-              pressTheme
-              title="Outfit Category"
-              subTitle="Categorize your outfit"
-              icon={<Ionicons name="book" />}
-              iconAfter={<Ionicons name="chevron-forward" />}
-              onPress={() => null}
-            />
-          </YGroup.Item>
-        </YGroup>
+          </XStack>
+          <YGroup>
+            <YGroup.Item>
+              <ListItem
+                themeInverse
+                pressTheme
+                fontSize={15}
+                fontFamily={"$span"}
+                icon={
+                  <Ionicons size={18} color={"#5D5D5D"} name="file-tray-full" />
+                }
+                iconAfter={<Ionicons size={18} name="chevron-forward" />}
+                onPress={() => setOpen(true)}
+              >
+                Tags
+              </ListItem>
+            </YGroup.Item>
+            <YGroup.Item>
+              <ListItem
+                pressTheme
+                themeInverse
+                fontSize={15}
+                fontFamily={"$span"}
+                icon={<Ionicons size={18} color={"#5D5D5D"} name="location" />}
+                iconAfter={<Ionicons size={18} name="chevron-forward" />}
+              >
+                Location
+              </ListItem>
+            </YGroup.Item>
+          </YGroup>
+          <YGroup>
+            <YGroup.Item>
+              <ListItem
+                themeInverse
+                title={"Allow comments"}
+                icon={<Ionicons size={18} color={"#5D5D5D"} name="chatbox" />}
+                subTitle={"Allow other users to comment on this post."}
+                iconAfter={
+                  <Switch
+                    size={"$3"}
+                    checked={checked}
+                    backgroundColor={checked ? "#FE9F10" : "$gray6Dark"}
+                    onCheckedChange={() => setChecked((curr) => !curr)}
+                  >
+                    <Switch.Thumb animation="bouncy" />
+                  </Switch>
+                }
+              />
+            </YGroup.Item>
+          </YGroup>
+        </YStack>
         <Button
           disabled={uploading}
           backgroundColor={"#FE9F10"}
@@ -244,6 +223,64 @@ export default function PreviewScreen({
           Post
         </Button>
       </YStack>
-    </KeyboardAwareScrollView>
+      <Sheet
+        modal
+        open={open}
+        dismissOnSnapToBottom
+        dismissOnOverlayPress
+        snapPoints={[90]}
+        onOpenChange={() => {
+          Keyboard.dismiss();
+          setOpen(false);
+        }}
+      >
+        <Sheet.Overlay />
+        <Sheet.Handle backgroundColor={"white"} />
+        <Sheet.Frame flex={1} backgroundColor={"white"}>
+          <YStack space padding="$3" flex={1}>
+            <Input
+              onChangeText={debounce}
+              backgroundColor={"$gray3Light"}
+              borderColor={"$gray3Light"}
+              focusStyle={{ borderColor: "$gray3Light" }}
+              color={"black"}
+            />
+            <XStack space="$1" flexWrap="wrap">
+              {topicNames.map((topicName, idx) => (
+                <Button
+                  marginBottom={"$1"}
+                  key={idx}
+                  size={"$2"}
+                  onPress={() =>
+                    setTopicNames((curr) =>
+                      curr.filter((_, index) => index !== idx)
+                    )
+                  }
+                  iconAfter={
+                    <Ionicons size={10} name="close" color={"white"} />
+                  }
+                  color={"black"}
+                  textProps={{
+                    color: "white",
+                    numberOfLines: 1,
+                  }}
+                >
+                  {topicName}
+                </Button>
+              ))}
+            </XStack>
+            <Separator borderWidth={1.5} borderColor={"$gray3Light"} />
+            <View flexGrow={1} minHeight={200}>
+              <FlashList<Topic>
+                data={data?.topics as Topic[]}
+                estimatedItemSize={50}
+                renderItem={renderItem}
+                keyExtractor={(_, idx) => idx.toString()}
+              />
+            </View>
+          </YStack>
+        </Sheet.Frame>
+      </Sheet>
+    </Pressable>
   );
 }
