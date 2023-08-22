bc410bb92d50fdfa562ff3172817527ef0132c0b
Pentavolt <amorphousei@gmail.com>
2023-08-22 16:19:00 +0200

    feature: add post image swiper

---
+++ components/ImageCarousel.tsx
@@ -0,0 +1,50 @@
+import ImageCarouselItem from "./ImageCarouselItem";
+import { Attachment } from "../generated/gql/graphql";
+import { View } from "tamagui";
+import { ListRenderItemInfo, useWindowDimensions } from "react-native";
+import { Ref } from "react";
+import { FlatList, TapGestureHandler } from "react-native-gesture-handler";
+import { useSharedValue, withTiming } from "react-native-reanimated";
+
+interface ImageCarouselProps {
+  attachments: Attachment[];
+  innerRef: Ref<any>;
+}
+
+export default function ImageCarousel({
+  attachments,
+  innerRef,
+}: ImageCarouselProps) {
+  const { width } = useWindowDimensions();
+  const fade = useSharedValue(1);
+
+  const onSingleTap = () => {
+    if (fade.value) fade.value = withTiming(0, { duration: 300 });
+    else fade.value = withTiming(1, { duration: 300 });
+  };
+
+  const renderItem = ({ item }: ListRenderItemInfo<Attachment>) => (
+    <ImageCarouselItem fade={fade} attachment={item} />
+  );
+
+  return (
+    <TapGestureHandler
+      waitFor={innerRef}
+      numberOfTaps={1}
+      onActivated={onSingleTap}
+    >
+      <View>
+        <FlatList<Attachment>
+          pagingEnabled
+          horizontal={true}
+          snapToAlignment="end"
+          snapToInterval={width}
+          keyExtractor={(_, idx) => idx.toString()}
+          data={attachments}
+          renderItem={renderItem}
+          decelerationRate={"normal"}
+        />
+      </View>
+    </TapGestureHandler>
+  );
+}
---
+++ components/ImageCarouselItem.tsx
@@ -0,0 +1,69 @@
+import FastImage from "react-native-fast-image";
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { AnimatableClothingLabel } from "./post/ClothingLabel";
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { Tag } from "../generated/gql/graphql";
+import { View } from "tamagui";
+
+interface CarouselItemProps {
+  fade: SharedValue<number>;
+  attachment: {
+    id: number;
+    tags: Tag[];
+    url: string;
+    height: number;
+    width: number;
+  };
+}
+
+const AnimatedClothingLabel = Animated.createAnimatedComponent(
+  AnimatableClothingLabel
+);
+
+export default function ImageCarouselItem({
+  attachment,
+  fade,
+}: CarouselItemProps) {
+  const { height, width } = useWindowDimensions();
+  const style = useAnimatedStyle(() => ({
+    opacity: fade.value,
+  }));
+
+  return (
+    <View height={height - 60} width={width}>
+      <Animated.View
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          flex: 1,
+          zIndex: 1,
+        }}
+      >
+        {attachment.tags.map((tag, idx) => {
+          const aspectRatio = attachment.height / attachment.width;
+          const actualHeight = aspectRatio * width;
+          return (
+            <AnimatedClothingLabel
+              key={idx}
+              photoWidth={width}
+              photoHeight={actualHeight}
+              windowHeight={height - 60}
+              tag={tag}
+              style={style}
+            />
+          );
+        })}
+      </Animated.View>
+      <FastImage
+        resizeMode="contain"
+        source={{ uri: attachment.url }}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </View>
+  );
+}
