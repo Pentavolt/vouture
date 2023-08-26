de62eee6503ad58734c84655a29205a6db8af528
Pentavolt <amorphousei@gmail.com>
2023-08-26 15:43:43 +0200

    fix: remove fragment

--- components/post/ActionPanel.tsx
+++ components/post/ActionPanel.tsx
@@ -43,63 +43,61 @@ export default function ActionPanel({
   const isSaved = post.saves.some((save) => save.userId === user?.id);
 
   return (
-    <>
-      <XStack
-        width={"100%"}
-        backgroundColor={"black"}
-        height={60}
-        paddingHorizontal={"$3"}
-        justifyContent="space-between"
-        alignItems="center"
+    <XStack
+      width={"100%"}
+      backgroundColor={"black"}
+      height={60}
+      paddingHorizontal={"$3"}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Button
+        backgroundColor={"$backgroundTransparent"}
+        onPress={onLikePress}
+        icon={
+          <Ionicons
+            size={25}
+            color={isLiked ? "#ef4444" : "white"}
+            name={isLiked ? "heart" : "heart-outline"}
+          />
+        }
       >
+        {post.likes.length.toString()}
+      </Button>
+      <Button
+        backgroundColor={"$backgroundTransparent"}
+        icon={<Ionicons color="white" size={25} name="chatbubble-outline" />}
+        onPress={onCommentPress}
+      >
+        {post.comments.length.toString()}
+      </Button>
+      {!post.poll && (
         <Button
           backgroundColor={"$backgroundTransparent"}
-          onPress={onLikePress}
+          onPress={onSavePress}
           icon={
             <Ionicons
               size={25}
-              color={isLiked ? "#ef4444" : "white"}
-              name={isLiked ? "heart" : "heart-outline"}
+              color={isSaved ? "#FE9F10" : "white"}
+              name={isSaved ? "bookmark" : "bookmark-outline"}
             />
           }
         >
-          {post.likes.length.toString()}
+          {post.saves.length.toString()}
         </Button>
+      )}
+      {post.poll && (
         <Button
           backgroundColor={"$backgroundTransparent"}
-          icon={<Ionicons color="white" size={25} name="chatbubble-outline" />}
-          onPress={onCommentPress}
-        >
-          {post.comments.length.toString()}
-        </Button>
-        {!post.poll && (
-          <Button
-            backgroundColor={"$backgroundTransparent"}
-            onPress={onSavePress}
-            icon={
-              <Ionicons
-                size={25}
-                color={isSaved ? "#FE9F10" : "white"}
-                name={isSaved ? "bookmark" : "bookmark-outline"}
-              />
-            }
-          >
-            {post.saves.length.toString()}
-          </Button>
-        )}
-        {post.poll && (
-          <Button
-            backgroundColor={"$backgroundTransparent"}
-            onPress={onPollPress}
-            icon={<Ionicons size={25} color={"red"} name={"compass-outline"} />}
-          />
-        )}
-        <Button
-          backgroundColor={"$backgroundTransparent"}
-          onPress={onSharePress}
-          icon={<Ionicons size={25} name={"share-outline"} />}
+          onPress={onPollPress}
+          icon={<Ionicons size={25} color={"red"} name={"compass-outline"} />}
         />
-      </XStack>
-    </>
+      )}
+      <Button
+        backgroundColor={"$backgroundTransparent"}
+        onPress={onSharePress}
+        icon={<Ionicons size={25} name={"share-outline"} />}
+      />
+    </XStack>
   );
 }
