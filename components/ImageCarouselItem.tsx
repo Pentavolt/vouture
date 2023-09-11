a3853dbbc1c78fbe996b27c1aa26c3c73b4af246
Pentavolt <amorphousei@gmail.com>
2023-09-11 09:07:36 +0200

    fix: offset height with safe area insets on iOS

--- components/ImageCarouselItem.tsx
+++ components/ImageCarouselItem.tsx
@@ -7,6 +7,7 @@ import { AnimatableClothingLabel } from "./post/ClothingLabel";
 import { StyleSheet, useWindowDimensions } from "react-native";
 import { Tag } from "../generated/gql/graphql";
 import { View } from "tamagui";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface CarouselItemProps {
   fade: SharedValue<number>;
@@ -28,12 +29,13 @@ export default function ImageCarouselItem({
   fade,
 }: CarouselItemProps) {
   const { height, width } = useWindowDimensions();
+  const { top, bottom } = useSafeAreaInsets();
   const style = useAnimatedStyle(() => ({
     opacity: fade.value,
   }));
 
   return (
-    <View height={height - 60} width={width}>
+    <View flex={1} width={width}>
       <Animated.View
         style={{
           ...StyleSheet.absoluteFillObject,
@@ -49,7 +51,7 @@ export default function ImageCarouselItem({
               key={idx}
               photoWidth={width}
               photoHeight={actualHeight}
-              windowHeight={height - 60}
+              windowHeight={height - top - bottom - 60}
               tag={tag}
               style={style}
             />
